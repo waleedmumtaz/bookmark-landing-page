9270157e970cf735088a6356d756eb56cdc808f9
Waleed <waleedthedev@gmail.com>
2022-01-15 20:06:02 +0500

    Added active states to faqs section

---
+++ src/lib/arrow-icon.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let iconStroke = '#5267DF'
+  export let iconStyles = ''
+
+  // iconStyles === '' ? 'rotate-180' : ''
+</script>
+
+<svg width="18" height="12" class={`${iconStyles} ml-2`}
+  ><path
+    fill="none"
+    stroke={iconStroke}
+    stroke-width="3"
+    d="M1 1l8 8 8-8"
+  /></svg
+>
--- src/lib/faqs.svelte
+++ src/lib/faqs.svelte
@@ -1,4 +1,58 @@
-<div class="mb-40">
+<script>
+  import ArrowIcon from './arrow-icon.svelte'
+
+  let arrowIconsStyles = [
+    {
+      styles: '',
+      stroke: '#5267DF',
+    },
+    {
+      styles: '',
+      stroke: '#5267DF',
+    },
+    {
+      styles: '',
+      stroke: '#5267DF',
+    },
+    {
+      styles: '',
+      stroke: '#5267DF',
+    },
+  ]
+
+  const toggleFaq = (faqNumber) => {
+    switch (faqNumber) {
+      case 'questionOne':
+        arrowIconsStyles[0].styles =
+          arrowIconsStyles[0].styles === '' ? 'rotate-180' : ''
+        arrowIconsStyles[0].stroke =
+          arrowIconsStyles[0].stroke === '#5267DF' ? '#FA5757' : '#5267DF'
+        break
+      case 'questionTwo':
+        arrowIconsStyles[1].styles =
+          arrowIconsStyles[1].styles === '' ? 'rotate-180' : ''
+        arrowIconsStyles[1].stroke =
+          arrowIconsStyles[1].stroke === '#5267DF' ? '#FA5757' : '#5267DF'
+        break
+      case 'questionThree':
+        arrowIconsStyles[2].styles =
+          arrowIconsStyles[2].styles === '' ? 'rotate-180' : ''
+        arrowIconsStyles[2].stroke =
+          arrowIconsStyles[2].stroke === '#5267DF' ? '#FA5757' : '#5267DF'
+        break
+      case 'questionFour':
+        arrowIconsStyles[3].styles =
+          arrowIconsStyles[3].styles === '' ? 'rotate-180' : ''
+        arrowIconsStyles[3].stroke =
+          arrowIconsStyles[3].stroke === '#5267DF' ? '#FA5757' : '#5267DF'
+        break
+      default:
+        break
+    }
+  }
+</script>
+
+<div class="mb-40 bgcstm">
   <div class="px-8">
     <div class="text-center">
       <h2 class="font-bold text-2xl">Frequently Asked Questions</h2>
@@ -12,16 +66,19 @@
     <div class="mt-10">
       <!-- Question 1 -->
       <details>
-        <summary class="flex justify-between items-center py-4 border-b-2">
-          What is Bookmark? <span
-            ><img
-              src="/assets/icon-arrow.svg"
-              alt="arrow icon"
-              class="ml-2"
-            /></span
-          >
+        <summary
+          on:click={() => {
+            toggleFaq('questionOne')
+          }}
+          class="flex justify-between items-center py-4 border-b-2"
+        >
+          What is Bookmark?
+          <ArrowIcon
+            iconStroke={arrowIconsStyles[0].stroke}
+            iconStyles={arrowIconsStyles[0].styles}
+          />
         </summary>
-        <p class="text-sm pt-3 pb-6 mr-5">
+        <p class="text-sm py-4 mr-5 leading-8">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
           tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis
           quam ornare mattis.
@@ -29,16 +86,19 @@
       </details>
       <!-- Question 2 -->
       <details>
-        <summary class="flex justify-between items-center py-4 border-b-2">
-          How can I request a new browser? <span
-            ><img
-              src="/assets/icon-arrow.svg"
-              alt="arrow icon"
-              class="ml-2"
-            /></span
-          >
+        <summary
+          on:click={() => {
+            toggleFaq('questionTwo')
+          }}
+          class="flex justify-between items-center py-4 border-b-2"
+        >
+          How can I request a new browser?
+          <ArrowIcon
+            iconStroke={arrowIconsStyles[1].stroke}
+            iconStyles={arrowIconsStyles[1].styles}
+          />
         </summary>
-        <p class="text-sm pt-3 pb-6 mr-5">
+        <p class="text-sm py-4 mr-5 leading-8">
           Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa,
           ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros
           aliquet convallis ultricies. Mauris augue massa, ultricies non ligula.
@@ -48,16 +108,19 @@
       </details>
       <!-- Question 3 -->
       <details>
-        <summary class="flex justify-between items-center py-4 border-b-2">
-          Is there a mobile app? <span
-            ><img
-              src="/assets/icon-arrow.svg"
-              alt="arrow icon"
-              class="ml-2"
-            /></span
-          >
+        <summary
+          on:click={() => {
+            toggleFaq('questionThree')
+          }}
+          class="flex justify-between items-center py-4 border-b-2"
+        >
+          Is there a mobile app?
+          <ArrowIcon
+            iconStroke={arrowIconsStyles[2].stroke}
+            iconStyles={arrowIconsStyles[2].styles}
+          />
         </summary>
-        <p class="text-sm pt-3 pb-6 mr-5">
+        <p class="text-sm py-4 mr-5 leading-8">
           Sed consectetur quam id neque fermentum accumsan. Praesent luctus
           vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula
           quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex
@@ -66,16 +129,19 @@
       </details>
       <!-- Question 4 -->
       <details>
-        <summary class="flex justify-between items-center py-4 border-b-2">
-          What about other Chromium browsers? <span
-            ><img
-              src="/assets/icon-arrow.svg"
-              alt="arrow icon"
-              class="ml-2"
-            /></span
-          >
+        <summary
+          on:click={() => {
+            toggleFaq('questionFour')
+          }}
+          class="flex justify-between items-center py-4 border-b-2"
+        >
+          What about other Chromium browsers?
+          <ArrowIcon
+            iconStroke={arrowIconsStyles[3].stroke}
+            iconStyles={arrowIconsStyles[3].styles}
+          />
         </summary>
-        <p class="text-sm pt-3 pb-6 mr-5">
+        <p class="text-sm py-4 mr-5 leading-8">
           Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat
           mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque
           non ut velit.
